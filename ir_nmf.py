bf0843affc120f4d34a3f729457daca5177d0618
AbnerLinux <abner.kahan77@myhunter.cuny.edu>
2021-09-01 15:41:19 -0400

    ok first commit

--- ir_nmf.py
+++ ir_nmf.py
@@ -2,5 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import cauchy
+irtest1= np.array(np.linspace(400,4000,num=36000))
 
-irtest1= np.array(np.linspace(400-4000,num=36000))
+

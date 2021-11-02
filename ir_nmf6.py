2bfd28b8b916d39eb0f003c9d6020356e8c7ef77
AbnerLinux <abner.kahan77@myhunter.cuny.edu>
2021-11-02 16:43:21 -0400

    first error plot

--- ir_nmf6.py
+++ ir_nmf6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import matplotlib.pyplot as plt
 import numpy as np
-#import pandas as pd
+import pandas as pd
 import scipy.stats as stats
 import random
 #from scipy import constants
@@ -64,7 +64,9 @@ def IrPlotter(item,title):
     plt.clf()
     
     
-def nmf2TesterMix(fraction1,numPoints):
+def nmf2TesterMix(numPoints):
+    fraction1 = random.random()
+    print(f'The expected fraction is {fraction1:.3}')
     IRF = np.zeros((2,numPoints))
     IR0 = addIRS(8,numPoints)
     IrPlotter(IR0,"FirstPlot")
@@ -189,7 +191,7 @@ def nmfTesterMix(fracList,numPoints):
         
 
 
-         plt.gca().invert_yaxis()
+         plt.gca().inNMF22vert_yaxis()
          plt.legend(["Original", "Calculated"])
          plt.title(str(entri[0][0])+ " Both Spectra")
          plt.xlabel("cm^-1")
@@ -198,4 +200,68 @@ def nmfTesterMix(fracList,numPoints):
 
 #nmfTesterMix([1,.5,.25,0,.8,.3],10000)
 # =============================================================================
-nmf2TesterMix(.32,10000)
+#nmf2TesterMix(10000)
+
+def NMF2TestRapid(numPoints):
+    fraction1 = random.random()
+    print(f'The expected fraction is {fraction1:.3}')
+    IRF = np.zeros((2,numPoints))
+    IR0 = addIRS(8,numPoints)
+    IR1 = addIRS(8,numPoints)
+    IRF[0,:] = IR0
+    IRF[1,:] = IR1
+    IRF= np.transpose(IRF)
+    
+    IrMix = np.zeros((3,numPoints))
+    IrMix[0,:]=IR0
+    IrMix[1,:]=IR1
+    IrMix[2,:] = IR0*fraction1 + IR1*(1-fraction1) 
+    #IrMix[3,:] = IR0*(1-fraction2)  + IR1*fraction1
+    IrMix= np.transpose(IrMix)
+    model = NMF(n_components=2, init='nndsvd',  max_iter=15000, tol= 1*10**-7)
+    #it seems that mu gives more close results
+    #must analyze errors and create plots
+    W = model.fit_transform(IrMix)
+    H = model.components_
+    W2 = np.matmul(W,H)
+    return np.sum(abs(IrMix-W2))/IrMix.size
+    #print(W)
+    #HO = H.copy()
+    #print(H)
+   # H = np.apply_along_axis(lambda l :l/np.amax(l) ,1,H)
+    
+#     #print ("taco", IrMix[:,0:10])
+#     print ("squash", W2[:,0:10])
+#     ##print(H)
+# #   IrPlotter(W[:,0], "First Calc Spectra")
+#    # IrPlotter(W[:,1], "Second Calc Spectra")
+#     #print (np.mean(np.where(W[:,1]>0))/np.mean((np.where(W[:,0]>0)))
+#     #print(model.fit(IrMix))
+# # =============================================================================
+#     for entri in nmfMatcher(IRF,W):
+        
+#           #plt.plot(np.linspace(0,1000,numPoints),IRF[:,entri[0][0]],color="red")
+#           if H[0,0]>.01:
+#               print(f'The calculated fraction of the first is {H[0,2]:.3}.')
+#               print(f'The calculated fraction of the second is {H[1,2]:.3}.')
+
+             
+#           else:
+#               print(f'The calculated fraction of the first is {H[1,2]:.3}')
+#               print(f'The calculated fraction of the second is, {H[0,2]:.3}.')
+#          #plt.plot(np.linspace(0,1000,numPoints),(W[:,entri[1][0]]*(max(HO[entri[1][0]]))))
+#         # print("full", (max(HO[entri[0][0]])))
+        
+
+
+#        #  plt.gca().invert_yaxis()
+#         # plt.legend(["Original", "Calculated"])
+        # plt.title(str(entri[0][0])+ " Both Spectra")
+        # plt.xlabel("cm^-1")
+        # plt.show()
+        # plt.clf()
+
+firstRun = pd.DataFrame(data={}, columns=['Fraction','Error'])
+for n in range (1000):
+    firstRun.loc[len(firstRun.index)] = NMF2TestRapid(10000)
+firstRun.to_csv('/home/abnerkahan/github/NMFBeginning/firstRun.csv')

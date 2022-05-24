88932d95a9a08b56bd74d25c75ff2c763eb6db87
Abner-Kahan <abner.kahan77@myhunter.cuny.edu>
2022-05-24 10:33:45 -0400

    test4

--- NMFglycan.py
+++ NMFglycan.py
@@ -17,11 +17,12 @@ from sklearn.decomposition import NMF
 from scipy.signal import savgol_filter
 from scipy.signal import find_peaks
 
-mapGL2 =  open('fun.txt', 'r')
-maptext = mapGL2.readlines()  
+mapGL2 =  open('100inc.txt', 'r')
+maptext = mapGL2.read()  
+print(maptext)
 mapGL2.close()
 #print(maptext)
 print(type(maptext))
-print(type(maptext[0]))
-contactList = maptext[0].split("{")
+#print(type(maptext[0]))
+contactList = maptext.split("{")
 print(len(contactList))

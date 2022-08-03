82e3529ad5690777a60def941a67e3d5f36323c0
Abner-Kahan <56643696+Abner-Kahan@users.noreply.github.com>
2022-08-03 13:55:34 -0400

    to other cpu

--- BinaryJuly.py
+++ BinaryJuly.py
@@ -15,16 +15,18 @@ def positionmap(file, residues):
     reader =  distancefile.read()
     
     superlist = reader.split ('} {')
-    
+    #print(superlist [-50:])
+    #print("\n\n\n")
     superlist [0] = superlist[0].replace('{', '')
     superlist [-1] = superlist[-1].replace('}', '')
     superlist [-1] = superlist[-1][:-1]
-    print(len(superlist))
+    #print(superlist [-50:])
+    #print(len(superlist))
     #print(superlist[-9:])
     distancefile.close()
      
     frames = int(len(superlist)/residues)
-    contactNP = np.zeros((residues **2, frames ))
+    contactNP = np.zeros((residues ** 2, frames ))
     reset  =  residues ** 2                  
     counter = 0
     fra = 0                   
@@ -33,14 +35,14 @@ def positionmap(file, residues):
         #print(entry)
         if  counter < reset:
             for bob in entry:
-                #print(bob)
+                #print(bob, counter)
                 contactNP [counter,fra] = int(bob)
                 counter +=1
         else:
             counter = 0
             fra +=1
             for bob in entry:
-                #print(bob)
+                #print(bob, counter)
                 contactNP [counter,fra] = int(bob)
                 counter +=1
 
@@ -51,8 +53,14 @@ def positionmap(file, residues):
     zerolist =[]
     for i in range(residues):
         zerolist.append(i*residues+i)
-    bonds = [(0,1), (1,2), (2,3), (3,4), (4,5), (2,6), (6,7) ,(7,8)]
-    for bond in bonds:
+    #G2bonds = [(0,1), (1,2), (2,3), (3,4), (4,5), (2,6), (6,7) ,(7,8)]
+    #G2fbonds = [(0,1), (0,2), (2,3), (3,4), (4,5), (5,6), (3,7),  (7,8), (8,9)  ]
+    M9bonds = [ (0,1), (1,2),(2,3),(2,8), (8,9), (9,10), (3,6), (6,7),(3,4), (4,5)]
+    M9bondsB = [ (0,1), (1,2),(2,3),(2,8), (8,9), (9,10), (3,6), (6,7),(3,4), (4,5), (1,3),(3,5), (8, 10) ]
+    #N2bonds = [(0,1), (1,2), (2,3), (3,4), (2,5), (5,6)]\
+   # N2fbonds = [(0,1), (0,2), (2,3), (3,6), (6,7), (3,4), (4,5)   ]
+    #S2bonds = [(0,1), (1,2), (2,3), (3,4),(4,5), (5,6), (2,7), (7,8), (8,9), (9,10)  ]
+    for bond in M9bondsB:
         zerolist.append((bond[0]*residues)+bond[1])
         zerolist.append((bond[1]*residues)+bond[0])
     print(zerolist)
@@ -62,24 +70,28 @@ def positionmap(file, residues):
     
     return contactNP
 def nmfMap(distancemap):   
-    model = NMF(n_components = 2, max_iter=1000, tol= 1*10**-10, solver= 'mu', beta_loss= 'kullback-leibler')
+    model = NMF(n_components =2, max_iter=1200, tol= 1*10**-10, solver= 'mu', beta_loss= 'kullback-leibler', init ='random')
     W = model.fit_transform(distancemap)
     H = model.components_
     for n in range (2):
         plt.plot(W[:,n])
     plt.title("Protein Binary Components")
-    plt.legend(["Component 1","Component 2"])
+    #plt.legend(["Component 1"])
+    #plt.legend(["Component 1"])
+
     plt.show()
     plt.clf()
     for indy in range(2):
          plt.plot(H[indy,:], linewidth=.3)
-         plt.title('G2 Component ' + str(indy +1))
+         plt.title('S2 Component ' + str(indy +1))
          plt.xlabel("Frame")
          plt.show()
          plt.clf()  
-    #plt.legend(["Component 1","Component 2"])
+    #plt.legend(["Component 1"])
     print(H.shape, "H")
     print(W.shape, "W")
     np.save('tempCompons.npy', H)
+    np.save('ProCompons.npy', W)
+    
         
-nmfMap(positionmap('vmd/g2-inc.txt',9 ))
+nmfMap(positionmap('vmd/m9-45d.txt',11 ))
--- analyzeResidues.py
+++ analyzeResidues.py
@@ -13,17 +13,17 @@ def moveAverage(L,n):
     return np.convolve(L, np.ones(n), 'valid') / n
 print("bob")
 H = np.load('tempCompons.npy')
-W =np.load('ProCompons.npy')
-H2 = np.zeros((2, H.shape[1]-499))
-for entry in range(2):
+W = np.load('ProCompons.npy')
+H2 = np.zeros((1, H.shape[1]-499))
+for entry in range(1):
     H2[entry,:] = moveAverage(H[entry,:], 500)
-print(np.argmin(H2, 1))
-H_sort = np.sort(H2, 1)
+#print(np.argmin(H2, 1))
+#H_sort = np.sort(H2, 1)
 
-newList = []
-for val in H_sort[0,:10]:
-    place = (np.where(H2 == val))[1]
-    newList += list(place)
+#newList = []
+#for val in H_sort[0,:10]:
+#    place = (np.where(H2 == val))[1]
+#    newList += list(place)
 #newarray2 = newarray
 #print(newList)
 #for n in range(len(newList)):
@@ -32,33 +32,116 @@ for val in H_sort[0,:10]:
    #     if abs(newList[n] - newList[b])<25:
    #         newList[b] = 0
 #print(newList)
-uniqList = []
-preVal = -2000
-for i in newList:
-    if abs (i  - preVal) > 1000:
-        uniqList.append(i)
-        preVal = i
-peaks = (find_peaks(W[:,1]))[0]
-print(peaks)
+#uniqList = []
+#preVal = -2000
+#for i in H_sort[0, :]:
+ #   if abs (i  - preVal) > 1000:
+ #       uniqList.append(i)
+ #       preVal = i
+Hpeaks =  find_peaks(H2[0,:], prominence=.005)
+
+print(Hpeaks)
+print ("----------------\n")
+print (find_peaks(H2[0,:]*-1, prominence=.005))
+
+print ("----------------\n")
+peaks = find_peaks(W[:,0], height = .7)
+#print(peaks)
+
+
+
+
+
 tupleList = []
-for n in peaks:
-     tupleList += [ tuple( [n // 11, n % 11 ])]
-print(tupleList)
-    
-    
+for n in peaks[0]:
+     tupleList += [ tuple( [ (n // 11) + 1, (n % 11) + 1 ])]
+index = 0
+for (x,y) in zip(peaks[0], peaks[1]['peak_heights']) :
+        label = tupleList[index]
+       #  if index %2 == 0:
+       #      y = y + .5
+       #      x = x -5
+       # # else:
+       #  #    x = x +4
+        if index == 1:
+             x = x-2
+       #  if label == (4,2):
+       #      x = x -3
+        if index ==3:
+            x-=1
+        if index == 4 :
+             x+=4
+        if index == 5:
+             x+=1
+        if index == 6 :
+             y+=0
+        if index == 7 :
+             x+=6
+
+       #  if index == 8:
+       #      x+=5
+       #  if index == 10:
+       #      x+=5
+            
+       # if index == 10:
+       #      y  = y - 1
+       # if index == 7:
+        #    y = y -.5
+       #     x = x - 4
+        plt.annotate(label, # this is the text
+             (x,y), # these are the coordinates to position the label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,3), # distance from text to points (x,y)
+                 ha='center', color= "green") # horizontal alignment can be left, right or center
+        index += 1
+# peaks2 = find_peaks(W[:,1], height = 0.5)
+# #print(peaks2)
+# tupleList2 = []
+# for n in peaks2[0]:
+#       tupleList2 += [ tuple( [ (n // 11) + 1, (n % 11) + 1 ])]
+# index = 0
+# for (x,y) in zip(peaks2[0], peaks2[1]['peak_heights']) :
+#         label = tupleList2[index]
+#         if index == 3:
+#             x = x +4
+        
+#         plt.annotate(label, # this is the text
+#               (x,y), # these are the coordinates to position the label
+#                   textcoords="offset points", # how to position the text
+#                   xytext=(0,1), # distance from text to points (x,y)
+#                   ha='center', color= "red") # horizontal alignment can be left, right or center
+        
+#         index += 1
+        
+
+plt.plot(W[:,0], color='green')
+#plt.plot(W[:,1], color='red')
+#plt.plot(W[:,1], color ='red')
+#plt.ylim(0,10)
+
+plt.title("Protein Binary Components - Component 0")
+#plt.legend(['Comp 1'])
+
+#plt.legend(peaks, tupleList)
+plt.show()
+plt.clf()
 
     
     
-    
 
-    
     
 #for search in range (0,10000,10):
 #search= 
 #    print(H[0, search], search)
 #print(search)
-for n in range(2):
-    plt.plot(np.linspace(0, len(H2[0,:]), len(H2[0,:])), H2[n,:])
-    plt.title("G2 Component "+ str(n))
+for n in range(1):
+    plt.plot(np.linspace(0, len(H2[n,:]), len(H2[n,:])), H2[n,:])
+    plt.title("S2 Component "+ str(n))
+    plt.show()
+    plt.clf()
+
+for n in range(1):
+    plt.plot(np.linspace(0, len(H[n,:]), len(H[n,:])), H[n,:])
+    plt.title("S2 Component "+ str(n))
     plt.show()
     plt.clf()

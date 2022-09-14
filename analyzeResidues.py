3c2ab9e727e8a27237e797976d874687defbecf0
AbnerLinux <abner.kahan77@myhunter.cuny.edu>
2022-09-14 15:57:26 -0400

    did m9

--- analyzeResidues.py
+++ analyzeResidues.py
@@ -15,9 +15,9 @@ def moveAverage(L,n):
 print("bob")
 H = np.load('tempCompons.npy')
 W = np.load('ProCompons.npy')
-H2 = np.zeros((numNMF, H.shape[1]-2499))
+H2 = np.zeros((numNMF, H.shape[1]-99))
 for entry in range(numNMF):
-    H2[entry,:] = moveAverage(H[entry,:], 2500)
+    H2[entry,:] = moveAverage(H[entry,:], 100)
 #print(np.argmin(H2, 1))
 #H_sort = np.sort(H2, 1)
 
@@ -39,17 +39,17 @@ for entry in range(numNMF):
  #   if abs (i  - preVal) > 1000:
  #       uniqList.append(i)
  #       preVal = i
-Hpeaks =  find_peaks(H2[0,:], prominence=.05)
+Hpeaks =  find_peaks(H2[0,:], prominence=.01)
 #print(Hpeaks[0])
 #for (p,z) in zip (Hpeaks[0], Hpeaks[1]['prominences']):
 #    print (p,z)
 #print ("----------------\n")
 #Hmins = find_peaks(H2[0,:]*-1, prominence=1.3)
 #print(Hmins[0])
-#for (p,z) in zip (Hmins[0], Hmins[1]['prominences']):
+#for (p,z) M6Bondsin zip (Hmins[0], Hmins[1]['prominences']):
  #   print (p,z)
 #print ("----------------\n")
-peaks = find_peaks(W[:,0], height = .2)
+peaks = find_peaks(W[:,0], height = .3)
 #print(peaks)
 
 
@@ -59,7 +59,7 @@ peaks = find_peaks(W[:,0], height = .2)
 tupleList = []
 for n in peaks[0]:
      print(n)
-     tupleList += [ tuple( [ (n // 12) + 1, (n % 12) + 1 ])]
+     tupleList += [ tuple( [ (n // 11 + 1), (n % 11 + 1 )])]
 index = 0
 for (x,y) in zip(peaks[0], peaks[1]['peak_heights']) :
         label = tupleList[index]
@@ -108,7 +108,7 @@ if numNMF == 2:
     peaks2 = find_peaks(W[:,1], height = 0.5)
     tupleList2 = []
     for n in peaks2[0]:
-          tupleList2 += [ tuple( [ (n // 12) + 1, (n % 12) + 1 ])]
+          tupleList2 += [ tuple( [ (n // 11) + 1, (n % 11) + 1 ])]
     index = 0
     for (x,y) in zip(peaks2[0], peaks2[1]['peak_heights']):
             label = tupleList2[index]
@@ -125,13 +125,13 @@ if numNMF == 2:
     plt.plot(W[:,1], color='red')
         
 
-plt.plot(np.linspace(0,12**2,144), W, color='green')
+plt.plot(np.linspace(0,11**2,121), W, color='green')
 #plt.ylim(0,1)
 
 #plt.plot(W[:,1], color ='red')
 #plt.ylim(0,10)
 
-plt.title("G3F Binary Components - Component 0")
+plt.title("M9 Binary Components - Component 0")
 plt.xlabel("First residue in contact with second residue")
 #plt.legend(['Comp 1'])
 
@@ -149,14 +149,14 @@ plt.clf()
 #print(search)
 for n in range(numNMF):
     plt.plot(np.linspace(0, len(H2[n,:]), len(H2[n,:])), H2[n,:], linewidth=.8)
-    plt.title("G3F Component "+ str(n))
+    plt.title("M9 Component "+ str(n))
     plt.xlabel("Frame")
     plt.show()
     plt.clf()
 
 for n in range(numNMF):
     plt.plot(np.linspace(0, len(H[n,:]), len(H[n,:])), H[n,:])
-    plt.title("G3F Component "+ str(n))
+    plt.title("M9 Component "+ str(n))
     plt.show()
     plt.clf()
     
@@ -164,13 +164,28 @@ for n in range(numNMF):
 #for n in range (50):
  #   print(.02+n/100, np.where(abs(H - .02-n/100) <.01))
  
+#numpy.random.choice
+def myRandom(npAr):
+    if npAr.size > 0:
+        return np.random.choice(npAr)
+    else:
+        return ""
+    
+#for n in range (50):
+ #   print(0+n/100, myRandom(np.where(abs(H - 0-n/100) <.01)[1]))
+
+
+# for n in [7947,92768,85962, 64950 ,53011 ,83369 ,2078 , 45832 ,55691 ,57406 ,56477]:
+#     print(f"H[{n}] = {H[0, n]}")
  
  #np.where(abs(H[0,20000:]-.38)<.01)
-counts = []
-for n in range (int(np.ceil(np.max(H))*10)+1):
-    counts.append(np.count_nonzero(np.logical_and (H[0] <= n/10, H[0] > (n-1)/10 )))
-print(counts)
-plt.bar(np.linspace(0,3, 31), counts, log = True)
-plt.xlabel('value')
-plt.ylabel('Frequency')
-     
+# =============================================================================
+# counts = []
+# for n in range (int(np.ceil(np.max(H))*10)+1):
+#     counts.append(np.count_nonzero(np.logical_and (H[0] <= n/10, H[0] > (n-1)/10 )))
+# print(counts)
+# plt.bar(np.linspace(0,3, 31), counts, log = True)
+# plt.xlabel('value')
+# plt.ylabel('Frequency')
+#      
+# =============================================================================
